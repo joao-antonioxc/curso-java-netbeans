9288c2f82d1b6b83d26800aec41b4f19612e96aa
João Antonio <jao.ratones@gmail.com>
2022-11-02 09:34:48 -0300

    a

---
+++ OperadoresAritmeticos/src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,11 @@
+package com.mycompany.operadoresaritmeticos;
+
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1+n2)/2;
+        System.out.println("A média do aluno é " + m);
+    }
+}
